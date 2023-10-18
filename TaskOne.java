eb063cbf5ef67780a6c0bd35e9f91c9caa8a85f0
Botari745 <hydrokinetics745@gmail.com>
2023-10-18 20:25:33 +0300

    HometaskJava_1

---
+++ TaskOne.java
@@ -0,0 +1,14 @@
+public class TaskOne {
+    public static void main(String[] args) {
+        int raspberry;
+        raspberry = 34;
+
+        int currant;
+        currant = 2 * raspberry;
+
+        int answerOne;
+        answerOne = currant - raspberry;
+
+        System.out.println("Cмородини зібрали на " + answerOne + " кг більше");
+    }
+}
---
+++ TaskTwo.java
@@ -0,0 +1,14 @@
+public class TaskTwo {
+    public static void main(String[] args) {
+        float bagOne ;
+        bagOne = 54.4F;
+        float bagTwo;
+        bagTwo = bagOne / 1.7F;
+        float bagThree;
+        bagThree = 2.6F * bagTwo;
+        float answerTwo;
+        answerTwo = bagOne + bagTwo + bagThree;
+
+        System.out.println("В трьох мішках " + answerTwo + " кг");
+    }
+}
